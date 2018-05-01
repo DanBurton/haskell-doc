e5730ca0da56cc2453d8a2c3bb6efcf91d7dad64
Dan Burton <danburton.email@gmail.com>
2018-05-01 11:33:48 -0600

    Added yesod-minimal scaffold

---
+++ haskelldocs/src/Home.hs
@@ -0,0 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes       #-}
+module Home where
+
+import Foundation
+import Yesod.Core
+
+getHomeR :: Handler Html
+getHomeR = defaultLayout $ do
+    setTitle "Minimal Multifile"
+    [whamlet|
+        <p>
+            This site is under construction.
+            Please enjoy the following minimal yesod scaffold.
+            (If you are curious about this site, contact Dan Burton for info.)
+        <p>
+            <a href=@{AddR 5 7}>HTML addition
+        <p>
+            <a href=@{AddR 5 7}?_accept=application/json>JSON addition
+    |]
